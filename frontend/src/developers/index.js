f6fbb883c1f7d6879a0ea54d8a2e6af7137c82ae
us-invest-personal <us.invest.personal.2020@gmail.com>
2023-09-25 19:14:35 +0200

    Ejercicio 1 - completo
    Propuesta de ejercicio 1 para Práctica 3

---
+++ frontend/src/developers/index.js
@@ -0,0 +1,48 @@
+import {
+    Table,
+  } from "reactstrap";
+  import useFetchState from "../util/useFetchState"; //
+  
+export default function DeveloperList() {
+    const [developers, setDevelopers] = useFetchState(
+        [],
+        `/api/v1/developers`
+      );
+
+    const imgnotfound = "https://cdn-icons-png.flaticon.com/512/48/48639.png";
+
+    const developerList =
+    developers.map((d) => {
+        return (
+            <tr key={d.id}>
+                <td className="text-center">{d.name}</td>
+                <td className="text-center"> {d.email} </td>
+                <td className="text-center"> {d.url} </td>
+                <td className="text-center"> 
+                  <img src={d.properties.picUrl ? d.properties.picUrl : imgnotfound } alt={d.name} width="50px"/>    
+                </td>
+            </tr>
+        );
+    });
+
+    return (
+        <div>
+      <div className="admin-page-container">
+        <h1 className="text-center">Developers</h1>
+        <div>
+          <Table aria-label="clinics" className="mt-4">
+            <thead>
+              <tr>
+                <th className="text-center">Name</th>
+                <th className="text-center">e-mail</th>
+                <th className="text-center">URL</th>
+                <th className="text-center">Picture</th>
+              </tr>
+            </thead>
+            <tbody>{developerList}</tbody>
+          </Table>
+        </div>
+      </div>
+    </div>
+    );
+  }
